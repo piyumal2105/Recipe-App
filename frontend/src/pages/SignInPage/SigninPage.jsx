21f00bb9059dd7e51f72e27b2d3c12470fc91a9b
Piyumal Madhuwantha <121705632+piyumal2105@users.noreply.github.com>
2024-12-07 14:23:43 +0530

    commit

--- frontend/src/pages/SignInPage/SigninPage.jsx
+++ frontend/src/pages/SignInPage/SigninPage.jsx
@@ -52,9 +52,9 @@ function SignInPage() {
           <div className="flex-1">
             <Link to="/" className="font-bold dark:text-white text-4xl">
               <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-                Flavor
+                Foodie
               </span>
-              Fusion
+              Hub
             </Link>
             <p className="text-sm my-5">
               You can sign in with your email and password or with Google
--- frontend/src/pages/SignUpPage/SignupPage.jsx
+++ frontend/src/pages/SignUpPage/SignupPage.jsx
@@ -69,9 +69,9 @@ function SignUpPage() {
           <div className="flex-1">
             <Link to="/" className="font-bold dark:text-white text-4xl">
               <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-                Flavor
+                Foodie
               </span>
-              Fusion
+              Hub
             </Link>
             <p className="text-sm my-5">
               You can sign up with your email and password or with Google.
